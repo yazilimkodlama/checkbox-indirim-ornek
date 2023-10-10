57eef7869a7d16dd1087cb7c74c8f695b7d44c80
yazilimkodlama <145126274+yazilimkodlama@users.noreply.github.com>
2023-10-10 14:48:10 +0300

    Add files via upload

---
+++ indirimli-satis/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace indirimli_satis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkOgrenci = new System.Windows.Forms.CheckBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblOdenecek = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adet";
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(120, 29);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(210, 29);
+            this.txtAdet.TabIndex = 1;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(120, 64);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(210, 29);
+            this.txtFiyat.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fiyat";
+            // 
+            // chkOgrenci
+            // 
+            this.chkOgrenci.AutoSize = true;
+            this.chkOgrenci.Location = new System.Drawing.Point(120, 99);
+            this.chkOgrenci.Name = "chkOgrenci";
+            this.chkOgrenci.Size = new System.Drawing.Size(210, 28);
+            this.chkOgrenci.TabIndex = 4;
+            this.chkOgrenci.Text = "Öğrenci (%20 İndirim)";
+            this.chkOgrenci.UseVisualStyleBackColor = true;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(120, 133);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(210, 44);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblOdenecek
+            // 
+            this.lblOdenecek.AutoSize = true;
+            this.lblOdenecek.Location = new System.Drawing.Point(116, 193);
+            this.lblOdenecek.Name = "lblOdenecek";
+            this.lblOdenecek.Size = new System.Drawing.Size(157, 24);
+            this.lblOdenecek.TabIndex = 6;
+            this.lblOdenecek.Text = "Ödenecek Tutar: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 258);
+            this.Controls.Add(this.lblOdenecek);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.chkOgrenci);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkOgrenci;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblOdenecek;
+    }
+}
+
---
+++ indirimli-satis/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace indirimli_satis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double adet, fiyat, odenecek;
+
+            adet=Convert.ToDouble(txtAdet.Text);
+            fiyat=Convert.ToDouble(txtFiyat.Text);
+
+            //if(chkOgrenci.Checked )
+            //{
+            //    odenecek = (adet * fiyat) * 0.80;
+            //}
+            //else
+            //{
+            //    odenecek = adet * fiyat;
+            //}
+
+            odenecek = adet * fiyat;
+
+            if(chkOgrenci.Checked ) //Öğrenci işaretliyse
+            {
+                odenecek = odenecek * 0.8; //%20 indirim uygula
+            }
+
+
+            lblOdenecek.Text="Ödenecek Tutar: "+odenecek.ToString();
+        }
+    }
+}
